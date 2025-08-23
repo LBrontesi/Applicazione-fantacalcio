e2411d8f880046def7af8942c413aae48b749239
Lorenzo Brontesi <bro@MacBook-Air-di-Lorenzo-2.local>
2025-08-23 18:32:33 +0200

    ciao

--- web_scraping.py
+++ web_scraping.py
@@ -1,60 +1,72 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import csv
-import pandas as pd
+import time
 
-ruoli = ['Portieri',
-       'Difensori',
-        'Centrocampisti', 'Trequartisti', 'Attaccanti'
-         ]
+ruoli = ['Portieri', 'Difensori', 'Centrocampisti', 'Trequartisti', 'Attaccanti']
 
-# Carica il file Excel con i dati dei calciatori
-df = pd.read_excel("C:/Users/bront/OneDrive/Desktop/Quotazioni_Fantacalcio_Stagione_2024_25.xlsx", header=1)
-listName = df['Nome']
-df.set_index("Nome", inplace=True)
+def safe_text(tag, default="N/A"):
+    return tag.get_text().strip() if tag else default
+
 calciatori = []
 for ruolo in ruoli:
-   html = requests.get("https://www.fantacalciopedia.com/lista-calciatori-serie-a/" + ruolo.lower() + "/")
-   soup =  bs(html.content,"html.parser")
-   g = soup.find_all("div", class_= "col_full giocatore")
-   for t in g:
-       calciatori.append(t.find("a").get("href"))
+    url = f"https://www.fantacalciopedia.com/lista-calciatori-serie-a/{ruolo.lower()}/"
+    try:
+        html = requests.get(url, timeout=10)
+        soup = bs(html.content, "html.parser")
+        calciatori += [a.get("href") for a in soup.select("div.col_full.giocatore a") if a.get("href")]
+    except Exception as e:
+        print(f"Failed to load {url}: {e}")
+print(calciatori)
+print("**********************************************")
+print(list(set(calciatori)))
+time.sleep(10)
 
-with open('asta1.csv', 'w', newline='') as f:
+with open('asta1.csv', 'w', newline='', encoding="utf-8") as f:
     writer = csv.writer(f)
-    field = ["Nome","ALG FTP", "FM 23-24", "FM 22-23", "FM 21-22 ", "attributi","Res. Inf."]
-    writer.writerow(field)
-    for y in calciatori:
-        print(y)
-        html2 = requests.get(y)
-        #estrai attributi
-        soup2 = bs(html2.content, "html.parser")
-        nome = soup2.find_all("div",class_="fancy-title title-bottom-border")[0].h1.get_text().strip().lower().capitalize()
-        tokens = nome.split()
-        parole_da_verificare = ['di', 'de', "del"]
-        if len(tokens) > 1 and tokens[0].lower() in parole_da_verificare:
-             nome = tokens[0].capitalize() + ' ' + tokens[1].capitalize()
-        else:
-            nome = tokens[0]
-        g = soup2.find("div", class_="col_full center mc_hookEvolution")
-        g = g.find_all("div", class_="col_one_fourth")
-        attrs = []
-        for j in g:
-            attrs.append(j.find("span", class_="stickdanpic").get_text().strip())
+    writer.writerow(["Nome", "ALG FTP", "FM 23-24", "FM 22-23", "FM 21-22", "attributi", "Res. Inf."])
+
+    for link in calciatori:
+        try:
+            html2 = requests.get(link, timeout=10)
+            soup2 = bs(html2.content, "html.parser")
+
+            # Extract player name
+            nome_div = soup2.find("div", class_="fancy-title title-bottom-border")
+            nome = safe_text(nome_div.h1 if nome_div and nome_div.h1 else None)
+            tokens = nome.split()
+            parole_da_verificare = ['di', 'de', 'del']
+            if len(tokens) > 1 and tokens[0].lower() in parole_da_verificare:
+                nome = f"{tokens[0].capitalize()} {tokens[1].capitalize()}"
+            elif tokens:
+                nome = tokens[0]
+            else:
+                nome = "N/A"
+
+            # Extract attributes
+            attrs_div = soup2.find("div", class_="col_full center mc_hookEvolution")
+            attrs = []
+            if attrs_div:
+                for div in attrs_div.find_all("div", class_="col_one_fourth"):
+                    span = div.find("span", class_="stickdanpic")
+                    attrs.append(safe_text(span))
+
+            # Extract fantavoto
+            fm_div = soup2.find("div", class_="col_three_fourth")
+            FM = []
+            if fm_div:
+                for div in fm_div.find_all("div", class_="col_one_fourth"):
+                    span = div.find("span", class_="stickdan")
+                    FM.append(safe_text(span))
+            while len(FM) < 4:
+                FM.append("N/A")
+
+            # Extract Res. Inf.
+            inf_divs = soup2.find_all("div", class_="counter counter-inherit counter-instant")
+            inf = safe_text(inf_divs[-1]) if inf_divs else "N/A"
+
+            writer.writerow([nome, FM[0], FM[1], FM[2], FM[3], attrs, inf])
 
 
-        #estrai medie fantavoto
-        g = soup2.find("div", class_="col_three_fourth")
-        g = g.find_all("div", class_="col_one_fourth")
-        FM = []
-        for j in g:
-            FM.append(j.find("span", class_="stickdan").get_text().strip())
-        g=soup2.find_all("div", class_="counter counter-inherit counter-instant")
-        inf = g[-1].text
-
-
-
-        writer.writerow([nome,FM[0],FM[1],FM[2],FM[3],attrs,inf])
-
-
-
+        except Exception as e:
+            print(f"Failed to scrape {link}: {e}")
